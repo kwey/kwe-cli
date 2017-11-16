aa9ae7838f1c7c3f47b4470622f573ccb902b641
雪人 <zhangjingqiang@bilibili.com>
2017-11-16 18:42:59 +0800

    update: init

--- src/download.js
+++ src/download.js
@@ -1,17 +1,21 @@
+const download = require('download-git-repo');
+const update = require('./update');
+const exists = require('fs').existsSync;
+const shell = require('shelljs')
 
-var download = require('download-git-repo');
-var update = require('./update');
-
-module.exports = function (options, cb) {
-    var branch = options.branch || 'master';
-    var name = options.name || 'kwe_template';
-    console.log(123123123123)
-    download('KWEY/kwe_template#' + branch, name, { clone: true }, function (err) {
+module.exports = function(options, cb) {
+    const branch = options.branch || 'master';
+    const name = options.name || 'kwe_template';
+    if (exists(options.name)) {
+        shell.rm('-rf', options.name)
+    }
+    download('KWEY/kwe_template#' + branch, name, {
+        clone: true
+    }, function(err) {
         console.log(err ? 'Error' : 'Success')
         if (!err) {
+            update(options)
             cb(name);
-            update(name)
         }
     })
 }
-
--- src/update.js
+++ src/update.js
@@ -1,12 +1,27 @@
-const shell = require('shelljs')
+const shell = require('shelljs');
+const fs = require('fs');
+const path = require('path');
+const jsonfile = require('jsonfile')
 
-module.exports = function (name) {
-    if (!name) {
+function replacePackage(options) {
+    const file = './package.json'
+    let pkg = JSON.parse(fs.readFileSync('./package.json'), 'utf8');
+    for (const key in options) {
+        if (options.hasOwnProperty(key) && key !== 'branch') {
+            pkg[key] = options[key];
+        }
+    }
+    jsonfile.writeFile(file, pkg, {spaces: 2, EOL: '\r\n'}, function (err) {
+        console.error(err)
+    })
+}
+module.exports = function (options) {
+    if (!options.name) {
         console.log('no file')
     }
-    shell.cp('-rf', name + '/*', './')
-    remove(name)
-
+    shell.cp('-rf', options.name + '/*', './')
+    remove(options.name)
+    replacePackage(options)
     function remove(name) {
         shell.rm('-rf', name)
     }
